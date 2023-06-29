441098ae84739160a0725a9dcf7fda0e77165327
gldaniil <daniil.gajfullin@yandex.ru>
2023-06-30 00:03:01 +0500

    6 kyu: 6. Split Strings.js

--- 6 kyu/6. Split Strings.js
+++ 6 kyu/6. Split Strings.js
@@ -1,6 +1,7 @@
+// https://www.codewars.com/kata/515de9ae9dcfc28eb6000001
+
 function solution(str) {
   let pairs = [];
-
   for (let i = 0; i < str.length; i += 2) {
     let pair = str.slice(i, i + 2);
     if (pair.length === 1) {
@@ -8,7 +9,6 @@ function solution(str) {
     }
     pairs.push(pair);
   }
-
   return pairs;
 }
 
