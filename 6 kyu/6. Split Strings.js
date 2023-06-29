7d911e2b5599a778cd7618510bf953c39e095032
gldaniil <daniil.gajfullin@yandex.ru>
2023-06-29 23:58:36 +0500

    6 kyu: 6. Split Strings.js

---
+++ 6 kyu/6. Split Strings.js
@@ -0,0 +1,15 @@
+function solution(str) {
+  let pairs = [];
+
+  for (let i = 0; i < str.length; i += 2) {
+    let pair = str.slice(i, i + 2);
+    if (pair.length === 1) {
+      pair += "_";
+    }
+    pairs.push(pair);
+  }
+
+  return pairs;
+}
+
+console.log(solution("abcde"));
